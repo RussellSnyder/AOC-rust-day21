dc1ad12d0c69310300e35234e071832e1f4bfebf
Frank Blendinger <frank.blendinger@methodpark.de>
2020-12-09 17:10:45 +0100

    Add solution for day 4 (with broken part 2)
    Somehow rescued from Git history in
    https://github.com/dnnr/advent-of-code-2020
    We overwrote the part 1 solution with the (even wrong) solution for part
    2. This adds boths in parallel, but part 2 still needs to be fixed (we
    had luck on some input).

---
+++ src/day04.rs
@@ -0,0 +1,229 @@
+use regex::Regex;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+// part 1
+
+pub fn part1(inp: String) {
+    let passports = parse_file_part1(&inp);
+
+    let valid_count = passports.iter().filter(|x| is_valid_part1(x)).count();
+    println!("Valid (part 1): {}", valid_count);
+}
+
+fn parse_file_part1(inp: &str) -> Vec<Vec<String>> {
+    inp.split("\n\n")
+        .filter(|line| line.len() > 0)
+        .map(|line| line.replace("\n", " "))
+        .map(|line| parse_passport(&line))
+        .collect::<Vec<Vec<String>>>()
+}
+
+fn parse_passport(passport: &str) -> Vec<String> {
+    passport
+        .split(" ")
+        .map(|word| word.split(":").next().unwrap())
+        .map(|word| word.to_owned())
+        .collect::<Vec<String>>()
+}
+
+fn is_valid_part1(passport: &Vec<String>) -> bool {
+    let valid_fields = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+    let fields: HashSet<String> = HashSet::from_iter(passport.iter().cloned());
+
+    for valid_field in valid_fields {
+        if !fields.contains(valid_field) {
+            return false;
+        }
+    }
+
+    true
+}
+
+// part 2
+
+pub fn part2(inp: String) {
+    let passports = parse_file(&inp);
+
+    let valid_count = passports.iter().filter(|x| **x).count();
+    println!("Valid (part 2): {}", valid_count);
+}
+
+fn parse_file(inp: &str) -> Vec<bool> {
+    inp.split("\n\n")
+        .filter(|line| line.len() > 0)
+        .map(|line| line.replace("\n", " "))
+        .map(|line| validate_passport(&line))
+        .collect::<Vec<bool>>()
+}
+
+fn validate_passport(passport: &str) -> bool {
+    let haircolor_checker = HairColorChecker::new();
+    let pid_checker = PidChecker::new();
+
+    let fields = passport
+        .split(" ")
+        .map(|x| x.to_owned())
+        .collect::<Vec<String>>();
+
+    if !has_all_fields(
+        &fields
+            .iter()
+            .map(|x| x.split(":").next().unwrap().to_owned())
+            .collect::<Vec<String>>(),
+    ) {
+        return false;
+    }
+
+    fields
+        .iter()
+        .map(|word| validate_field(word, &haircolor_checker, &pid_checker))
+        .all(|x| x)
+}
+
+fn validate_field(
+    field: &str,
+    haircolor_checker: &HairColorChecker,
+    pid_checker: &PidChecker,
+) -> bool {
+    let name_value = field.split(":").collect::<Vec<&str>>();
+
+    if name_value.len() != 2 {
+        return false;
+    }
+
+    let name = name_value[0];
+    let value = name_value[1];
+
+    match name {
+        "byr" => is_valid_birthyear(value),
+        "iyr" => is_valid_issueyear(value),
+        "eyr" => is_valid_expirationyear(value),
+        "hgt" => is_valid_height(value),
+        "hcl" => haircolor_checker.is_valid_haircolor(value),
+        "ecl" => is_valid_eyecolor(value),
+        "pid" => pid_checker.is_valid_pid(value),
+        "cid" => true,
+        _ => panic!("Argh, unknown field type: {}", name),
+    }
+}
+
+fn has_all_fields(passport: &Vec<String>) -> bool {
+    let valid_fields = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+    let fields: HashSet<String> = HashSet::from_iter(passport.iter().cloned());
+
+    for valid_field in valid_fields {
+        if !fields.contains(valid_field) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn is_valid_birthyear(value: &str) -> bool {
+    if value.len() != 4 {
+        return false;
+    }
+    let year = value.parse::<u16>().ok();
+    match year {
+        Some(year) => year <= 2002 && year >= 1920,
+        None => false,
+    }
+}
+
+fn is_valid_issueyear(field: &str) -> bool {
+    if field.len() != 4 {
+        return false;
+    }
+    let year = field.parse::<u16>().ok();
+    match year {
+        Some(year) => year <= 2020 && year >= 2010,
+        None => false,
+    }
+}
+
+fn is_valid_expirationyear(field: &str) -> bool {
+    if field.len() != 4 {
+        return false;
+    }
+    let year = field.parse::<u16>().ok();
+    match year {
+        Some(year) => year <= 2030 && year >= 2020,
+        None => false,
+    }
+}
+
+fn is_valid_height(field: &str) -> bool {
+    if field.ends_with("in") {
+        let inch = field.split("in").next().unwrap().parse::<u16>().ok();
+        return match inch {
+            Some(inch) => inch <= 76 && inch >= 59,
+            None => false,
+        };
+    }
+    if field.ends_with("cm") {
+        let cm = field.split("cm").next().unwrap().parse::<u16>().ok();
+        return match cm {
+            Some(cm) => cm <= 193 && cm >= 150,
+            None => false,
+        };
+    }
+
+    false
+}
+
+struct HairColorChecker {
+    regex: Regex,
+}
+
+impl HairColorChecker {
+    fn new() -> Self {
+        let regex = Regex::new("^#[0-9a-f]{6}$").unwrap();
+        Self { regex }
+    }
+
+    fn is_valid_haircolor(&self, field: &str) -> bool {
+        self.regex.is_match(field)
+    }
+}
+
+struct PidChecker {
+    regex_works: Regex,
+    regex_worksnt: Regex,
+}
+
+impl PidChecker {
+    fn new() -> Self {
+        let regex_works = Regex::new("[0-9]{9}").unwrap(); // works but shouldn't
+        let regex_worksnt = Regex::new("^[0-9]{9}$").unwrap(); // worksn't but should
+        Self {
+            regex_works,
+            regex_worksnt,
+        }
+    }
+
+    fn is_valid_pid(&self, field: &str) -> bool {
+        let ret_works = self.regex_works.is_match(field);
+        let ret_worksnt = self.regex_worksnt.is_match(field);
+        if ret_works != ret_worksnt {
+            // panic!("Wtf? {}", field);
+        }
+        ret_works
+    }
+}
+
+fn is_valid_eyecolor(field: &str) -> bool {
+    let eyecolors = vec!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+    for ec in eyecolors {
+        if ec == field {
+            return true;
+        }
+    }
+
+    // println!("Invalid eyecolor? {}", field);
+    false
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 pub mod day05;
 pub mod day07;
 pub mod day08;
@@ -18,6 +19,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         1 => (day01::part1, day01::part2), // driver: yogan, navigator: t-animal, Russell
         2 => (day02::part1, day02::part2), // driver: yogan, navigator: dnnr, Russell
         3 => (day03::part1, day03::part2), // driver: dnnr,  navigator: yogan, Russell
+        4 => (day04::part1, day04::part2), // driver: dnnr,  navigator: yogan, Russell
         5 => (day05::part1, day05::part2), // driver: yogan, navigator: dnnr
         7 => (day07::part1, day07::part2), // yogan
         8 => (day08::part1, day08::part2), // driver: yogan, navigator: dnnr
