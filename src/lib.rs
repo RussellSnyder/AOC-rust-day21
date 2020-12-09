352a0bcd66405437bf2b9343cdb8ad87cb46b4c9
Frank Blendinger <frank.blendinger@methodpark.de>
2020-12-09 16:13:35 +0100

    Add comments about drivers/navigators

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,12 @@ pub type DayFn = fn(String);
 
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
-        1 => (day01::part1, day01::part2),
-        2 => (day02::part1, day02::part2),
-        5 => (day05::part1, day05::part2),
-        7 => (day07::part1, day07::part2),
-        8 => (day08::part1, day08::part2),
-        9 => (day09::part1, day09::part2),
+        1 => (day01::part1, day01::part2), // driver: yogan, navigator: t-animal, Russell
+        2 => (day02::part1, day02::part2), // driver: yogan, navigator: dnnr, Russell
+        5 => (day05::part1, day05::part2), // driver: yogan, navigator: dnnr
+        7 => (day07::part1, day07::part2), // yogan
+        8 => (day08::part1, day08::part2), // driver: yogan, navigator: dnnr
+        9 => (day09::part1, day09::part2), // driver: dnnr,  navigator: yogan, Russell
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
