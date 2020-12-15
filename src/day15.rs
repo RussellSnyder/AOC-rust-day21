edda18c49c9938fb21e6e2e5a7930e8ad5ced884
Frank Blendinger <frank.blendinger@methodpark.de>
2020-12-15 16:44:42 +0100

    Make day 15 more space-efficient (get rid of Vec)

--- src/day15.rs
+++ src/day15.rs
@@ -16,42 +16,35 @@ fn read_input(inp: &String) -> &str {
     inp.split("\n").filter(|line| line.len() > 0).next().unwrap()
 }
 
-fn calculate_sequence(line: &str, limit: usize) -> Vec<usize> {
-    let mut sequence: Vec<usize> = line
+fn calculate_sequence(line: &str, limit: usize) -> Vec<u32> {
+    let sequence: Vec<u32> = line
         .split(",")
-        .map(|s| s.parse::<usize>().unwrap())
+        .map(|s| s.parse::<u32>().unwrap())
         .collect();
 
     // keys are numbers, values are last seen indices in sequence
-    let mut last_seen: HashMap<usize, usize> = HashMap::new();
+    let mut last_seen: HashMap<u32, u32> = HashMap::new();
 
     sequence.iter().enumerate().for_each(|(index, number)| {
-        last_seen.insert(*number, index); // TODO don't insert last num
+        last_seen.insert(*number, index as u32);
     });
 
+    let mut current_number = sequence[sequence.len() - 1];
+
     for index in sequence.len()..limit {
-        let current_number = sequence[index - 1];
         let maybe_last_index = last_seen.get(&current_number);
         match maybe_last_index {
             Some(last_index) => {
-                let age = index - last_index - 1;
-                sequence.push(age);
+                let age = index as u32 - last_index - 1;
+                current_number = age;
             }
             None => {
                 // never seen before
-                sequence.push(0);
+                current_number = 0
             }
         }
-        last_seen.insert(current_number, index - 1);
+        last_seen.insert(current_number, (index - 1) as u32);
     }
 
     sequence
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn set_value_with_mask() {}
 }
